375dc588caab25424aaf8f7d435f4f9a4eecb441
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-09-12 23:20:45 +0200

    fixed treeview, added search functionality

--- tkinterface.py
+++ tkinterface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Scrollbar, ttk
+from turtle import width
 from database import *
 
 #class for tkinter app
@@ -12,36 +13,89 @@ class App(tk.Tk):
         self.create_widgets()
 
     def create_widgets(self):
-        #split screen into two frames (left and right) one with buttons one with treeview expand on y axis
         self.left_frame = tk.Frame(self)
         self.left_frame.pack(side=tk.LEFT, fill=tk.Y)
         self.right_frame = tk.Frame(self)
-        self.right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx= 10, pady=10)
+        self.right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx= (10,0), pady=(0,7))
 
-        #create buttons
+        self.search_frame = tk.Frame(self.right_frame)
+        self.search_frame.pack(side=tk.TOP, fill=tk.X, padx=(0, 17), pady=(0, 6)) # 17 chosen to make scrollbar fit
+        self.tree_frame = tk.Frame(self.right_frame)
+        self.tree_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        
+        self.search_top_frame = tk.Frame(self.search_frame)
+        self.search_top_frame.pack(side=tk.TOP, fill=tk.X)
+        self.search_bottom_frame = tk.Frame(self.search_frame)
+        self.search_bottom_frame.pack(side=tk.BOTTOM, fill=tk.X) 
+
+
+        #create button
+        self.info_button = tk.Button(self.left_frame, text="Info", command=self.info)
         self.add_button = tk.Button(self.left_frame, text="Tilføj bog", command=self.add_book)
-        self.add_button.pack()
         self.delete_button = tk.Button(self.left_frame, text="Slet bog", command=self.delete_book)
-        self.delete_button.pack()
         self.update_button = tk.Button(self.left_frame, text="Opdater bog", command=self.update_book)
+
+        self.info_button.pack()
+        self.add_button.pack()
+        self.delete_button.pack()
         self.update_button.pack()
 
         #create treeview
-        self.tree = ttk.Treeview(self.right_frame, columns=("title", "author", "genre", "product_code"), show='headings')
-        self.tree.heading("title", text="Titel")
-        self.tree.heading("author", text="Forfatter")
-        self.tree.heading("genre", text="Genre")
-        self.tree.heading("product_code", text="Produktkode")
-        self.tree.pack(fill=tk.Y, expand=True)
+        columns = ("Titel", "Forfatter", "Genre", "Produktkode")
+        self.tree = ttk.Treeview(self.tree_frame, columns=columns, show='headings')
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH,  expand=True)
+        
+        #create scrollbar
+        vsb = ttk.Scrollbar(self.tree_frame, orient="vertical", command=self.tree.yview)
+        vsb.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.tree.configure(yscrollcommand=vsb.set)
+
+        #create search bar list
+        self.search_bars = []
+        #used instead of keypressed as it only gives update on change and not if no change occurs
+        self.search_sv = []
+
+        #create columns
+        for column in columns:
+            self.tree.heading(column, text=column)
+            self.tree.column(column, width=1)
+
+            #add search entry above for each column
+            sv = tk.StringVar()
+            sv.trace_add("write", self.search)
+            
+            search_bar = tk.Entry(self.search_bottom_frame, textvariable=sv)
+            search_bar.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+            #must be kept alive
+            self.search_sv.append(sv)
+            self.search_bars.append(search_bar)
 
         
+        #allow only numbers for product code
+        self.search_bars[3].configure(validate=tk.ALL, validatecommand=((self.register(self._entry_restrict_numbers)), '%P'))
+
+        #add search title in top search frame
+        search_title = tk.Label(self.search_top_frame, text="Søg efter bøger", font='Helvetica 15 bold')
+        search_title.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
         #populate treeview
         self.populate_tree()
+        
+    def _entry_restrict_numbers(self, P):
+        if P.isdigit() or P == "":
+            return True
+        else:
+            return False
 
     def populate_tree(self, search: Book_Search_Query=Book_Search_Query()):
         self.tree.delete(*self.tree.get_children())
-        for book in self.db.get_minbooks(search):
+        for book in self.db.get_minbooks(search, limit=50):
             self.tree.insert("", tk.END, values=(book.title, book.author, book.genre, book.product_code))
+
+    def info(self):
+        pass
 
     def add_book(self):
         pass
@@ -51,6 +105,14 @@ class App(tk.Tk):
 
     def update_book(self):
         pass
+    
+    def search(self, *args):
+        search = Book_Search_Query(
+            title=self.search_bars[0].get() or None, 
+            author=self.search_bars[1].get() or None, 
+            genre=self.search_bars[2].get() or None, 
+            product_code=self.search_bars[3].get() or None)
+        self.populate_tree(search)
 
 
 
